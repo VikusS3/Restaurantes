10892e93ca5fd8c76f674aeb7fa2337e0d59771b
VikusS3 <felix21soc@gmail.com>
2024-04-26 10:34:58 -0500

    add animations

--- boomerang-landing/tailwind.config.mjs
+++ boomerang-landing/tailwind.config.mjs
@@ -1,3 +1,4 @@
+import animations from '@midudev/tailwind-animations'
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -6,8 +7,11 @@ export default {
 			colors: {
 				"button": "#F4511F",
 				"secondary": "#DAD8D7"
+			},
+			supports: {
+				"no-scroll-driven-animations": "not(animation-timeline: scroll())",
 			}
 		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
